17447dcdcf8d5600c6fbd5f03cecd0642fcc50a7
YataDz <122659347+YataDz@users.noreply.github.com>
2023-06-12 21:08:40 +0700

    quiz

---
+++ Quiz_pertemuan_4.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quiz_pertemuan_4;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+import java.util.Scanner;
+public class Quiz_pertemuan_4 {
+
+    public static void main(String[] args) {
+        
+          Scanner input = new Scanner(System.in);
+          System.out.println("Masukkan variabel 1: ");
+          int variabel1 = input.nextInt();
+          System.out.println("Masukkan variabel 2: ");
+          int variabel2 = input.nextInt();         
+          System.out.println(" ");          
+          int variabel3 = (variabel1 + variabel2);
+          System.out.println("Variabel1 + Variabel2 = " +variabel3);
+          
+          System.out.println("Dimas Arya Pradipta Sutandyo + 22201194");
+    }
+}
